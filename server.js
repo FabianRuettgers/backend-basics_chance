706296bd20966167370e561c8a51aade5aa68c69
Fabian Ruettgers <fabulon78@gmail.com>
2023-08-03 10:46:22 +0200

    fix

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const chance = new Chance();
 const name = chance.name();
 const age = chance.age();
 const profession = chance.profession();
-console.log(
-  `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
-);
+
 export const server = createServer((request, response) => {
   response.statusCode = 200;
   response.end(
